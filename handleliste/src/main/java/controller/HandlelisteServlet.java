11a864c60fdb9982ca4f11cc5efbd130733b9adf
haggbart <mr.haggbart@gmail.com>
2020-10-12 11:54:41 +0200

    vis sql i console

--- handleliste/src/main/java/controller/HandlelisteServlet.java
+++ handleliste/src/main/java/controller/HandlelisteServlet.java
@@ -1,17 +1,14 @@
 package controller;
 
 
-import model.Item;
 import service.HandlelisteService;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "HandlelisteServlet", urlPatterns = "/handleliste")
 public class HandlelisteServlet extends HttpServlet {
