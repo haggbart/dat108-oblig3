4200bee3d570d87b4283134398882c7a1fb111a6
Roger Karlsen <mr.haggbart@gmail.com>
2020-10-15 23:06:23 +0200

    better

--- handleliste/src/main/java/no/hvl/dat108/handleliste/controller/LoginFilter.java
+++ handleliste/src/main/java/no/hvl/dat108/handleliste/controller/LoginFilter.java
@@ -1,6 +1,7 @@
 package no.hvl.dat108.handleliste.controller;
 
-import no.hvl.dat108.handleliste.Handleliste.*;
+import no.hvl.dat108.handleliste.Handleliste;
+import no.hvl.dat108.handleliste.Handleliste.UrlPattern;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -9,25 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static no.hvl.dat108.handleliste.helper.Sessions.getNewSession;
-import static no.hvl.dat108.handleliste.helper.Sessions.isLoggedIn;
-
 @WebFilter(filterName = "LoginFilter", urlPatterns = "/handleliste")
 public class LoginFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
 
-        if (isLoggedIn(httpRequest)) {
-            chain.doFilter(request, response);
+        String message;
+
+        if (request.getRequestedSessionId() != null && !request.isRequestedSessionIdValid()) {
+            message = "Utløpt sesjon, vennligst logg inn på nytt";
+        } else if (session == null || session.getAttribute("user") == null) {
+            message = Handleliste.Loc.NOT_LOGGED_IN;
+        } else {
+            chain.doFilter(req, resp);
             return;
         }
-        HttpSession session = getNewSession(httpRequest);
-
-        session.setAttribute("message", Loc.NOT_LOGGED_IN);
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        httpResponse.sendRedirect(UrlPattern.LOGIN);
+        if (session == null) session = request.getSession(true);
+        session.setAttribute("message", message);
+        response.sendRedirect(UrlPattern.LOGIN);
     }
 }
---
+++ handleliste/src/main/java/no/hvl/dat108/handleliste/controller/SessionListener.java
@@ -0,0 +1,65 @@
+package no.hvl.dat108.handleliste.controller;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener()
+public class SessionListener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    // Public constructor is required by servlet spec
+    public SessionListener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("context initialized");
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("session created");
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("session destroyed");
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute 
+         is added to a session.
+      */
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attribute
+         is replaced in a session.
+      */
+    }
+}
